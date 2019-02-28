9a80d2d7594cd8afd355c07afd8922035427a413
jstrickler <jstrickler@gmail.com>
2019-02-28 09:29:56 -0500

    added missing H5 data files

--- EXAMPLES/np_matrices.py
+++ EXAMPLES/np_matrices.py
@@ -4,9 +4,9 @@ import numpy as np
 m1 = np.matrix([[ 2,  4,  6],
         [10, 20, 30]])
 
-m2 = np.matrix([[ 1, 15],
-        [ 3, 25],
-        [ 5, 35]])
+m2 = np.matrix([[ 1, 15, 7],
+        [ 3, 25, 8],
+        [ 5, 35, 9]])
 
 print('m1 =>\n', m1)
 print()
@@ -23,4 +23,11 @@ print()
 print('m1.A.transpose() =>\n', m1.A.transpose())
 print()
 
-print('m1.A.transpose() * m2.A =>\n', m1.A.transpose() * m2.A)
+# print('m1.A.transpose() * m2.A =>\n', m1.A.transpose() * m2.A)
+
+print(m2.shape)
+
+im = np.linalg.inv(m2)
+
+print(im)
+
--- np_read_csv.py
+++ np_read_csv.py
@@ -2,11 +2,13 @@
 import numpy as np
 
 a = np.loadtxt(
-    '../DATA/columns_of_numbers.text',
-    usecols=[2, 3],
+    'DATA/columns_of_numbers.txt',
+    # usecols=[2, 3],
     skiprows=1,
+    dtype=np.int32
 )
 
 print(a)
 print(a.shape)
 print(a.size)
+
